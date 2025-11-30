e66af246321e25d3eea4a6ee97aa0b29039f705e
Karan Singh <devkaran.codes@gmail.com>
2025-11-30 23:02:40 +0530

    added cta card

---
+++ src/components/CTACard.jsx
@@ -0,0 +1,19 @@
+import Container from "./Container";
+
+const CTACard = () => {
+  return (
+    <Container>
+      <div className="flex flex-col items-center justify-between gap-4 rounded-md bg-linear-to-r from-neutral-800 via-neutral-800 to-orange-600 px-4 py-4 font-semibold tracking-tighter md:flex-row">
+        <h3 className="flex items-center text-white">
+          <span className="mr-2 inline-block h-2 w-2 animate-pulse rounded-full bg-orange-500 duration-300" />
+          Early Bird Sale
+        </h3>
+        <button className="flex cursor-pointer items-center rounded-full bg-orange-400 px-4 py-2 text-sm tracking-tight text-white hover:opacity-95">
+          Claim your spot
+        </button>
+      </div>
+    </Container>
+  );
+};
+
+export default CTACard;
