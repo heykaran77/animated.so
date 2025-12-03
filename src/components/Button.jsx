5c78735b763711c7bc46cdc00e61650d80303c2f
Karan Singh <devkaran.codes@gmail.com>
2025-12-03 19:52:42 +0530

    add react icons and style btn

---
+++ src/components/Button.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <button className="mx-auto flex cursor-pointer items-center rounded-full bg-orange-400 px-4 py-2 text-xs tracking-tight text-white hover:opacity-95 md:text-sm">
+      We also design, develop & launch mobile apps
+    </button>
+  );
+};
+
+export default Button;
